33aaff7621549fc48527892c7a59f46d3433e1b3
马恩源 <maenyuan@tenqent.com>
2025-02-10 16:33:23 +0800

    feat(components): 切换全屏组件

---
+++ src/components/Themes/FullScreen.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps({
+  tag: {
+    type: String,
+    default: 'i',
+  },
+})
+
+const { isFullscreen, toggle } = useFullscreen()
+</script>
+
+<template>
+  <component
+    :is="tag"
+    :class="[isFullscreen ? 'i-ri:fullscreen-exit-fill' : 'i-ep:full-screen', 'cursor-pointer text-xl']"
+    @click="toggle"
+  />
+</template>
+
+<style scoped></style>
