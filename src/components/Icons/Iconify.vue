533d565d8ac7af35a1ea9f3821f3405f0db58efd
codermey <18672267024@163.com>
2025-02-17 15:22:54 +0800

    feat(components): 增加Iconify组件

---
+++ src/components/Icons/Iconify.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { Icon, type IconProps } from '@iconify/vue'
+
+const props = defineProps<IconProps>()
+</script>
+
+<template>
+  <component :is="Icon" v-bind="props"></component>
+</template>
+
+<style scoped></style>
